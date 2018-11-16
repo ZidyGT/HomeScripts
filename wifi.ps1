704cfcc810ade78d21f58fae808ffc8b94c72462
ZidyGT <zidy@outlook.cz>
2018-11-16 20:26:01 +0100

    Add files via upload

---
+++ wifi.ps1
@@ -0,0 +1,7 @@
+while((Get-NetAdapter "Wi-fi 2").Status -ne "Up")
+	{
+		netsh wlan connect name="Polachovi"
+		Start-Sleep -Milliseconds 2250
+ 	}
+netsh wlan set profileparameter name="Polachovi" ConnectionMode=auto
+
